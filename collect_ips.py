0921f44efdb65a3e0566ae105483660a5a14360a
lyl7410 <279469402@qq.com>
2025-05-30 18:32:45 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -32,7 +32,7 @@ print(f"Target Subdomains: {TARGET_SUBDOMAINS}")
 # --- Existing IP Collection Logic ---
 # 目标URL列表
 urls = [
-    'https://api.uouin.com/cloudflare.html',
+    'https://cf.vvhan.com/',
     'https://ip.164746.xyz'
 ]
 ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
